b1e6b2f46c7f6806d949dbd94b78e928cf5e65d2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-17 17:20:52 +0530

    added sub method in Calc class from br1 branch

--- demoApplication/src/com/lti/model/Calc.java
+++ demoApplication/src/com/lti/model/Calc.java
@@ -4,4 +4,7 @@ public class Calc {
 	public void add(int num1, int num2){
 		System.out.println("Sum= "+(num1+num2));
 	}
+	public void subs(int num1,int num2){
+		System.out.println("Difference: "+(num1-num2));
+	}
 }
