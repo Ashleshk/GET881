038d11f4688f0dfd4f7fce254496f5ddade4a0c3
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-18 15:53:55 +0530

    deleted method

--- demoApplication/src/com/lti/model/Calc.java
+++ demoApplication/src/com/lti/model/Calc.java
@@ -7,9 +7,6 @@ public class Calc {
 	public void subs(int num1,int num2){
 		System.out.println("Difference: "+(num1-num2));
 	}
-	public void mul(int num1,int num2){
-		System.out.println("multiply: "+(num1*num2));
-	}
 	
 	
 	
