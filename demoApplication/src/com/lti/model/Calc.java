f17e629de3c0d4f0257e90fa077073dd01a5bd11
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-18 15:52:29 +0530

    update calc java

--- demoApplication/src/com/lti/model/Calc.java
+++ demoApplication/src/com/lti/model/Calc.java
@@ -4,13 +4,5 @@ public class Calc {
 	public void add(int num1, int num2){
 		System.out.println("Sum= "+(num1+num2));
 	}
-	public void subs(int num1,int num2){
-		System.out.println("Difference: "+(num1-num2));
-	}
-	public void mul(int num1,int num2){
-		System.out.println("multiply: "+(num1*num2));
-	}
-	
-	
-	
+
 }
