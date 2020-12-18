c71c590ce223e4459e225a83e4bc0eefaf3cc0c4
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-18 15:31:22 +0530

    removed Methods Mod and Mod2

--- demoApplication/src/com/lti/model/Calc.java
+++ demoApplication/src/com/lti/model/Calc.java
@@ -14,11 +14,5 @@ public class Calc {
 	public void div(int num1,int num2){
 		System.out.println("multiply: "+(num1/num2));
 	}
-	public void mod(int num1,int num2){
-		System.out.println("modulus: "+(num1%num2));
-	}
 	
-	public void mod2(int num1,int num2){
-		System.out.println("modulus: "+(num1%num2));
-	}
 }
